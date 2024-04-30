1aeea38a2c509cac5465983b6b01b2b800cc67e4
salvatorebriga <salvo.z2302@gmail.com>
2024-04-30 16:44:27 +0200

    define algorithm

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 "use strict"
+
+for (let i = 0; i < 100; i++){
+
+    let x = i + 1;
+
+    if(x % 3 === 0 && x % 5 === 0){
+        console.log('FizzBuzz');
+    }
+    else if(x % 3 === 0){
+        console.log('Fizz');
+    }else if(x % 5 === 0){
+        console.log('Buzz');
+    }else{
+        console.log(x);
+    }
+}
