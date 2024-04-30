3d09cb34715add89bad8219da520f48ce069174d
salvatorebriga <salvo.z2302@gmail.com>
2024-04-30 22:10:41 +0200

    bonus 1 and  2

--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
 "use strict"
 
+const row = document.querySelector('div.row');
+
 for (let i = 0; i < 100; i++){
+    
+    setTimeout(() => {
 
     let x = i + 1;
 
     if(x % 3 === 0 && x % 5 === 0){
-        console.log('FizzBuzz');
+        const element = `<div class="col-10 bg-FizzBuzz">FizzBuzz</div>`;
+        row.innerHTML += element;
     }
     else if(x % 3 === 0){
-        console.log('Fizz');
+        const element = `<div class="col-10 bg-fizz">Fizz</div>`;
+        row.innerHTML += element;
     }else if(x % 5 === 0){
-        console.log('Buzz');
+        const element = `<div class="col-10 bg-buzz">Buzz</div>`;
+        row.innerHTML += element;
     }else{
-        console.log(x);
+        const element = `<div class="col-10 bg-number">${x}</div>`;
+        row.innerHTML += element;
     }
+
+    }, 1000);
 }
